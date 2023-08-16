19f54ea7cbddb65b7f98b9c6449facdfa2e26f41
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-08-15 17:00:07 -0700

    got the temperature, title, humidity , wind and date to display. also fixed my code so its not a mixture of vanilla java and jquery just jquery.

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 var apikey = "d1b898bb9f32f112ac2ba505b3cdbd73";
 var city = "Sacramento"
-const cityInput = document.querySelector("#search-input");
-const searchButton = document.querySelector("#searchBtn");
+const searchButton = $("#searchBtn");
 var cityHistoryList = [];
 
 // This is getting information from the api and putting it into the console when you search for a city
 const currentWeatherChoice = (cityName) => {
-    const cityName = cityInput.value.trim();
+   
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}`
-    console.log(cityName)
-fetch(apiUrl)
-.then(function(response){
-    return response.json();
     
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+    var currentTitle = $("#current-title")
+    var currentDay = moment().format("M/D/YYYY");
+    currentTitle.text(`${cityName} (${currentDay})`);
+    $("#current-temp").text("Temperature: " + data.main.temp)
+    $("#current-humid").text("Humidity: " + data.main.humidity)
+    $("#current-wind").text("Wind Speed: " + data.wind.speed)
+   
+    console.log(data)
 })
+
 }
 
-searchButton.addEventListener("click", cityinfo);
+$("#search-form").on("submit",function(event) { 
+    event.preventDefault();
+    var cityName = $("#search-input").val().trim()
+    currentWeatherChoice(cityName)
+});
